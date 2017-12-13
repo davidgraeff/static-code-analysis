f770b5f54a8e49371e8d8d20b4a282b589d3a60d
David Graeff <david.graeff@web.de>
2017-12-14 00:43:30 +0100

    Replace first group occurence only

--- renameGroup.sh
+++ renameGroup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -type f -exec sed -i 's#<groupId>org\.openhab\.tools</groupId>#<groupId>com\.github\.davidgraeff\.static-code-analysis</groupId>#g' {} +
+find . -type f -exec sed -i '0,/<groupId>org\.openhab\.tools<\/groupId>/ s#<groupId>org\.openhab\.tools</groupId>#<groupId>com\.github\.davidgraeff\.static-code-analysis</groupId>#g' {} +
 find . -type f -exec sed -i 's#<version>0\.4\.0</version>#<version>0\.4\.0</version>#g' {} +
